fc3940f888190f906e61d0e174afbbb340cc083d
Libra Website Deployment Script <libra-doc-bot@users.noreply.github.com>
2020-12-02 19:42:05 +0000

    [skip ci] Deploy website
    Deploy website version based on 81cd79bc6ec7d061c51cb372be71ae8bac1f7000

--- docs/rustdocs/move_lang/command_line/sidebar-items.js
+++ docs/rustdocs/move_lang/command_line/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["DEFAULT_OUTPUT_DIR",""],["DEPENDENCY",""],["DEPENDENCY_SHORT",""],["OUT_DIR",""],["OUT_DIR_SHORT",""],["SENDER",""],["SENDER_SHORT",""],["SOURCE_MAP",""],["SOURCE_MAP_SHORT",""]],"fn":[["parse_address",""]]});
+initSidebarItems({"constant":[["COLOR_MODE_ENV_VAR",""],["DEFAULT_OUTPUT_DIR",""],["DEPENDENCY",""],["DEPENDENCY_SHORT",""],["OUT_DIR",""],["OUT_DIR_SHORT",""],["SENDER",""],["SENDER_SHORT",""],["SOURCE_MAP",""],["SOURCE_MAP_SHORT",""]],"fn":[["parse_address",""],["read_bool_env_var",""],["read_env_var",""]]});
